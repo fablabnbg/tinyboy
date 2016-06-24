b33d5b1943412848213a53addc6ec19c93bec6fd
Juergen Weigert <jw@owncloud.com>
2016-06-24 02:59:29 +0200

    now it works

--- sendtinygcode.py
+++ sendtinygcode.py
@@ -1,4 +1,4 @@
-#! /bin/python
+#! /usr/bin/python
 #
 # sendtinyboy.py -- most simpilistic gcode forwarder for my 3d printer
 #
@@ -28,6 +28,12 @@ count = 0
 tstamp = time.time()
 start_tstamp = tstamp
 
+# gobble away initial boiler plate output, if any
+seen = ser.readline()
+while len(seen):
+  print seen
+  seen = ser.readline()
+
 while True:
   line = fd.readline()
   count += len(line)
@@ -38,7 +44,7 @@ while True:
 
   if len(line) > 0:
     if verbose: print "out", line
-    line += "\r\n"
+    line += "\n"
     while len(line):
       n = ser.write(line)
       if n < 0: raise WriteError()
@@ -56,12 +62,12 @@ while True:
   if (now > tstamp + 10):
     bps = float(count) / (1 + now - start_tstamp) 
     eta = ''
+    elapsed = "%dmin" % (int((now - start_tstamp)/60))
     if (now > start_tstamp + 2*60):	# start eta calc after 2 min
       secs = int(float(total-count)/bps)
       min = int(secs/60)
-      secs = secs - 60*min
-      eta = "ETA %dmin %dsec" % (min, secs)
-    print "%.1f %%, %s" % (count * 100. / total, eta)
+      eta = "ETA %dmin" % (min)
+    print "%.1f %%, %s" % (count * 100. / total, elapsed)
     tstamp = now
     
 
