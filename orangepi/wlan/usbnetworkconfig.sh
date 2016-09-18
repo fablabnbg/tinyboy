19e6d4cf67ad70c56c4db7e3aa5d0580b5b55d75
Juergen Weigert <jnweiger@gmail.com>
2016-09-18 18:58:44 +0200

    bash syntax removed.

--- orangepi/wlan/usbnetworkconfig.sh
+++ orangepi/wlan/usbnetworkconfig.sh
@@ -6,20 +6,24 @@
 # set 2>1 > $o
 # echo DEVNAME = $DEVNAME >> $o
 
+if [ -z "$DEVNAME" ]; then
+  echo "ERROR: DEVNAME not set. "
+  echo "Usage: e.g.: DEVNAME=/dev/sda $0"
+  exit 0
+fi
 
 lastpart=$(ls -1 $DEVNAME* | sort -r | head -n 1)
 mountpoint=$(mktemp -d)
-mount $lastpart $mointpoint
+mount $lastpart $mountpoint
 outdir=$mountpoint/network/$(date +%Y%m%d%H%M%S)
-echo 255 > /sys/class/leds/*red*/brightness
+echo 255 > /sys/class/leds/red_led/brightness
 
-if [ -d $mountpoint/network ]; then
-  mkdir -p $outdir
-  wpa_cli status > $outdir/wpa_cli-status
-  (a=$(wpa_cli status | grep wpa_state ); echo -n "$a "; test "$a" = 'wpa_state=COMPLETED' && wpa_cli status | grep ip_addr || echo) > $outdir/status
-  cp /etc/wpa_supplicant/wpa_supplicant.conf $outdir
-  cat << EOF > $mountpoint/network/README.txt
-$HOSTNAME uses wpa_suppliacant to try join a wireless network.
+mkdir -p $outdir
+wpa_cli status > $outdir/wpa_cli-status
+(a=$(wpa_cli status | grep wpa_state ); echo -n "$a "; test "$a" = 'wpa_state=COMPLETED' && wpa_cli status | grep ip_addr || echo) > $outdir/status
+cp /etc/wpa_supplicant/wpa_supplicant.conf $outdir
+cat << EOF > $mountpoint/network/README.txt
+Host $HOSTNAME uses wpa_suppliacant to try join a wireless network.
 A usb stick can be used to monitor success or change the configuration.
 Whenever a usb stick is inserted, two actions are performed:
 
@@ -31,18 +35,18 @@ b) If present, wpa_supplicant.conf is copied to /etc/wpa_supplicant/wpa_supplica
 The red led is on while the stick is mounted read/write. You can safely remove the usb stick as soon as the red led is off again.
 EOF
 
-  if [ -f $mountpoint/network/wpa_supplicant.conf ]; then
-    set -x
-    exec 2>&1 > $outdir/log
-    cp $mountpoint/network/wpa_supplicant.conf /etc/wpa_supplicant
-    wpa_cli reconfigure
-    sleep 2
-    wpa_cli status
-    sleep 2
-    wpa_cli status
-    set +x
-  fi
+if [ -f $mountpoint/network/wpa_supplicant.conf ]; then
+  set -x
+  exec 2>&1 > $outdir/log
+  cp $mountpoint/network/wpa_supplicant.conf /etc/wpa_supplicant
+  wpa_cli reconfigure
+  sleep 2
+  wpa_cli status
+  sleep 2
+  wpa_cli status
+  set +x
 fi
-umount $mointpoint
-rmdir $mointpoint
-echo 0 > /sys/class/leds/*red*/brightness
+
+umount $mountpoint
+rmdir $mountpoint
+echo 0 > /sys/class/leds/red_led/brightness
