c50f3d21faa0738bbfaa736e09cf455c7210022e
Jürgen Weigert <jnweiger@gmail.com>
2016-09-18 18:26:22 +0200

    Update tinyboy.sh
    Instructive comments added

--- orangepi/tinyboy.sh
+++ orangepi/tinyboy.sh
@@ -1,4 +1,16 @@
 #! /bin/sh
+# tinyboy.sh
+# This script asumes 
+# a) that you have a DNS entry tinyboy and 
+# b) your ssh-key in the root-account of that tinyboy machine.
+# c) a gcode subdirectory, for uploading files.
+#
+# This script uploads all *.gcode from the current directory, (or the named *.gcode files) with rsync.
+# then opens a remote shell with a screen session. from this shell, you
+# can run sendtinygcode.sh with a gcode file of your choice.
+# You can log off without interrupting the print job. When you log in with tinyboy.sh again,
+# you will be placed in the same screen session, showing you the progress of the last print job.
+
 set -x
 if [ -z "$*" ]; then 
   rsync -v *.gcode root@tinyboy:gcode
