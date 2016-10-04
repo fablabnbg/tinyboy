7aa5c54add1947d1bc1f3032888bc3cdd2973b9d
Juergen Weigert <jw@owncloud.com>
2016-10-04 23:00:08 +0200

    tool to continue aborted prints. Measure the height of the printed part you have, and let gcodecut.py produce a gcode file containing of exactly the remainder.

--- pronterface/sendgcode_pronsole.sh
+++ pronterface/sendgcode_pronsole.sh
@@ -2,8 +2,12 @@
 #
 # sends gcode via pronsole.py or simply connects with pronsole
 
+dir=$(dirname $0)
+
 if [ -n "$*" ]; then
-python Printrun/pronterface.py -e connect -e block_until_online -e load "$1" -e print -e emonitor
+python $dir/Printrun/pronsole.py -e connect -e block_until_online -e load "$1" -e print -e emonitor
 else
-python Printrun/pronterface.py -e connect -e block_until_online -e M114
+set -x
+python $dir/Printrun/pronsole.py -e connect # -e block_until_online -e M114
 fi
+
