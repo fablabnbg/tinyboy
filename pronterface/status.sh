1a20113092ff60eb380e08bf029572fc23af0111
Juergen Weigert <jw@owncloud.com>
2016-10-04 02:50:04 +0200

    sh echo cannot do -e. use /bin/echo to be safe.

--- pronterface/status.sh
+++ pronterface/status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # pronterface and pronsole speak xmlrpc
-echo -e "POST /RPC2 HTTP/1.1\r\nContent-Length: 100\r\n\r\n<?xml version='1.0'?>\n<methodCall>\n<methodName>status</methodName>\n<params>\n</params>\n</methodCall>"  | nc localhost 7978
+/bin/echo -e "POST /RPC2 HTTP/1.1\r\nContent-Length: 100\r\n\r\n<?xml version='1.0'?>\n<methodCall>\n<methodName>status</methodName>\n<params>\n</params>\n</methodCall>"  | nc localhost 7978
